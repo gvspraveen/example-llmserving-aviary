3d10f912bca3fde1df057e04d3737e5b6c89ddec
mjconnor <mattc@anyscale.com>
2023-06-12 11:01:38 -0700

    Update query.py

--- Test/query.py
+++ Test/query.py
@@ -1,7 +1,7 @@
 import requests
 
-resp = requests.get(
-    "http://localhost:8000/query/amazon--LightGPT", params={"prompt": "Where is a good place to eat in San Francisco?","use_prompt_format":"true"}
+resp = requests.post(
+    "http://127.0.0.1:8000/query/amazon--LightGPT", json={"prompt": "Where is a good place to eat in San Francisco?","use_prompt_format":"true"}
 )
 print(resp.json())
 
